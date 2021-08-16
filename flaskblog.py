55010cdb1df235dda936a46b2e32a2daad6b0aa0
anandcr <anandcr635@gmail.com>
2021-08-16 16:33:08 +0530

    Added forms and validations

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask , render_template, url_for
+from forms import  RegistrationFrom,LoginForm
 #instantiate flask variable , __name__ is similar to main
 app = Flask(__name__)
+
+app.config['SECRET_KEY']= '779fd751a43373c448714eaa1669fb7a'
 
 posts =[
     {'author':'Anand CR',
@@ -22,6 +25,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='about')
+
+@app.route("/register")
+def register():
+    form =RegistrationFrom()
+    return render_template('register.html',title='Register',form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Register',form=form)
 
 
 if __name__ == '__main__':
