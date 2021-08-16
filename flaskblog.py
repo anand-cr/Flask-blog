3d4b397ae3c55c5afc5a290402d9eb2a396bc472
anandcr <anandcr635@gmail.com>
2021-08-16 14:31:16 +0530

    setting up template

---
+++ flaskblog.py
@@ -0,0 +1,28 @@
+from flask import Flask , render_template
+#instantiate flask variable , __name__ is similar to main
+app = Flask(__name__)
+
+posts =[
+    {'author':'Anand CR',
+     'title':'Post1',
+     'date':'April 20'
+     },
+    {'author':'Aravind CR',
+     'title':'Post 2',
+     'date':'April 20'}
+]
+
+
+#this decorator will handlethe backend
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='about')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
